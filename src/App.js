3c099cf7f7ef7e7e626769ed55e1af58aec7333b
Shilcof <robert.shilcof@gmail.com>
2021-03-01 12:09:31 +0000

    commit tree 923a135fbd17d63ff0ca1f1f8e2091a5fb4337b7

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React from 'react';
+import Person from './Person';
+
+export default class App extends React.Component {
+
+    state = {
+        people: []
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res=>res.json())
+            .then(json=>this.setState({people: json.people}))
+    }
+
+    renderPeople() {
+        return this.state.people.map(person=><Person name={person.name}/>)
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderPeople()}
+            </div>
+        )
+    }
+}
